e6ecfb1cfa62d08d6a08aa490d67a1a31e4ae643
baboon <342829672@qq.com>
2020-06-02 15:21:28 +0800

    stream groupBy

--- stream/src/main/java/CollectToMap.java
+++ stream/src/main/java/CollectToMap.java
@@ -1,10 +1,13 @@
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import pojo.Student;
 
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
+import javax.swing.text.html.Option;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * @description: TODO
@@ -23,7 +26,7 @@ public class CollectToMap {
         Student student3 = new Student(3,"张3",1);
         Student student4 = new Student(3,"张4",2);
         Student student5 = new Student(4,"张4",11);
-        Student student6 = new Student(5,"张4",1);
+        Student student6 = new Student(4,"张4",1);
         Student student7 = new Student(6,"张4",11);
 
 
@@ -37,24 +40,30 @@ public class CollectToMap {
 
 
 
-        Map<Integer, List<Student>> collect = students.stream().collect(Collectors.groupingBy(Student::getId));
+        Map<Integer, List<Student>> collect = students.stream().collect(groupingBy(Student::getId));
         /*toMap*/
 //        Map<String, Student> collect6 = students.stream().collect(Collectors.toMap(Student::getName, v -> v));
 
         /*分组计数*/
 
-        Map<Integer, Long> collect4 = students.stream().collect(Collectors.groupingBy(Student::getId, counting()));
+        Map<Integer, Long> collect4 = students.stream().collect(groupingBy(Student::getId, counting()));
 
-        Map<Integer, List<Student>> collect6 = students.stream().collect(groupingBy(Student::getId));
-        collect6.forEach((k,v)-> {
-            List<Student> collect1 = v.stream().peek(x -> x.setAge(null)).collect(Collectors.toList());
-        });
+        Map<Integer, Integer> collect6 = students.stream().collect(toMap(Student::getId, Student::getAge, (a, b)->a+b+1));
         /*瞎写的 好像分组后又取了其中一个值*/
-        Map<Integer, Set<String>> collect5 = students.stream().collect(Collectors.groupingBy(Student::getAge, Collectors.mapping(Student::getName, Collectors.toSet())));
 
-        collect5.forEach((k,v)-> System.out.println(k+"|"+v));
+        /*toMap并处理相同的key对应的value*/
+        Map<Integer, Set<String>> collect5 = students.stream().collect(groupingBy(Student::getAge, mapping(Student::getName, toSet())));
+
+        /*group分组并确保顺序再操作 2个对比*/
+        Map<Integer, List<Student>> collect7 = students.stream().sorted(Comparator.comparing(Student::getAge)).collect(groupingBy(Student::getId,LinkedHashMap::new, toList()));
+        Map<Integer, List<Student>> collect8 = students.stream().sorted(Comparator.comparing(Student::getAge)).collect(groupingBy(Student::getId));
 
 
+
+        /*分组并尝试一次性修改分组后的数据*/
+        LinkedHashMap<Integer, List<Student>> collect9 = students.stream().collect(groupingBy(Student::getId, LinkedHashMap::new, collectingAndThen(
+                toList(), (s -> s)
+        )));
 
 
         /*获取最大的*/
@@ -65,17 +74,48 @@ public class CollectToMap {
 
         /*获取平均值*/
 
-        Double collect1 = students.stream().collect(Collectors.averagingDouble(Student::getAge));
+        Double collect1 = students.stream().collect(averagingDouble(Student::getAge));
 
         /*取上面所有的*/
-        IntSummaryStatistics collect2 = students.stream().collect(Collectors.summarizingInt(Student::getAge));
+        IntSummaryStatistics collect2 = students.stream().collect(summarizingInt(Student::getAge));
         long sum = collect2.getSum();
         int min1 = collect2.getMin();
 
         /*拼接字符串*/
-       String join =students.stream().map(Student::getAge).map(String::valueOf).collect(Collectors.joining("|"));
+       String join =students.stream().map(Student::getAge).map(String::valueOf).collect(joining("|"));
 
        /*reduce求和*/
        Integer collect3 = students.stream().map(Student::getAge).reduce(0, Integer::sum);
+
+
+
+       /*分组后尝试操作包装后的收集器*/
+        Map<Integer, Student> collect10 = students.stream().collect(groupingBy(Student::getId, collectingAndThen(toList(), v -> v.get(0))));
+
+        Map<Integer, Student> collect11 = students.stream().collect(groupingBy(Student::getId, collectingAndThen(maxBy(Comparator.comparing(Student::getAge)), Optional::get)));
+
+
+
+
+        List<String> s1 = Arrays.asList("a", "bb", "cc", "ddd");
+
+        Map<Integer, String> r1 = s1.stream()
+                .collect(groupingBy(String::length, joining(",", "[", "]")));
+
+        System.out.println(r1); // {1=[a], 2=[bb,cc], 3=[ddd]}
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
 }
