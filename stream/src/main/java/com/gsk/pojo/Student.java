32ff6983306f2a073706efaf41492e61f77b670a
baboon <342829672@qq.com>
2020-06-22 10:10:59 +0800

    流

--- stream/src/main/java/com/gsk/pojo/Student.java
+++ stream/src/main/java/com/gsk/pojo/Student.java
@@ -8,7 +8,7 @@ public class Student implements Comparable<Student> {
 	private String name;
 	private int age;
  
-	public Student(int id, String name, int age) {
+	public Student(int id, String name,  int age) {
 		this.id = id;
 		this.name = name;
 		this.age = age;
